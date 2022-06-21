ed3289a2192c1dc129aa74245a31b8e7074c860f
Folarinde <folarinde.olusola@gmail.com>
2022-06-21 23:39:25 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var myName = "Folarinde Olusola"
+var height = "5 feet 7 inches tall"
+var country = "Nigeria"
+
+alert("My name is " + myName)
+alert("I am " + height)
+alert("My country is " + country)
